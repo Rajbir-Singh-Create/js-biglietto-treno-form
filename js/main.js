f1de2b28797974bbfc5546f347be4eb0591c8dbb
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2024-12-18 20:20:14 +0100

    feat: bonus exercise added

--- js/main.js
+++ js/main.js
@@ -11,31 +11,42 @@ const pricePerKm = 0.21;
 calculatePriceButton.addEventListener('click', function(event){
     event.preventDefault();
 
+    calculatePriceButton.disabled = true;
+    
+    const spinnerElement = calculatePriceButton.querySelector('#spinner')
+    const statusText = calculatePriceButton.querySelector('.status');
+    const originalStatusText = statusText.innerHTML;
+
+    spinnerElement.classList.toggle('d-none');
+    statusText.innerHTML = `Calcolo il prezzo del biglietto...`;
+
     kmToCommute = parseFloat(kmToCommute.value);
-    console.log(kmToCommute);
+    // console.log("Km da percorrere: ",kmToCommute);
 
     if(isNaN(kmToCommute)){
         console.log("valore non valido");
         alert("Inserisci un numero per favore"); // TODO design this element
     } else {
-        // console.log("Km da percorrere: ",kmToCommute);
-        ticketPrice = pricePerKm * kmToCommute;
-        // console.log("Prezzo del biglietto: ",ticketPrice);
+        setTimeout(() => {
+            ticketPrice = pricePerKm * kmToCommute;
+            // console.log("Prezzo del biglietto: ",ticketPrice);
 
-        // price applied for underage and elderly people
-        if(age.value === "underAge") {
-            underAgeDiscout = ticketPrice - ((ticketPrice * 20) / 100);
-            ticketPrice = underAgeDiscout.toFixed(2).replace(".",",");
-            console.log((`Il prezzo finale è di € ${ticketPrice}`));
-        } else if (age.value === "over65"){
-            over65Discount = ticketPrice - ((ticketPrice * 40) / 100);
-            ticketPrice = over65Discount.toFixed(2).replace(".",",");
-            console.log((`Il prezzo finale è di € ${ticketPrice}`));
-        } else {
-            ticketPrice = ticketPrice.toFixed(2).replace(".",",");
-            console.log((`Il prezzo finale è di € ${ticketPrice}`));
-        }
+            // price applied for underage and elderly people
+            if(age.value === "underAge") {
+                underAgeDiscout = ticketPrice - ((ticketPrice * 20) / 100);
+                ticketPrice = underAgeDiscout.toFixed(2).replace(".",",");
+            } else if (age.value === "over65"){
+                over65Discount = ticketPrice - ((ticketPrice * 40) / 100);
+                ticketPrice = over65Discount.toFixed(2).replace(".",",");
+            } else {
+                ticketPrice = ticketPrice.toFixed(2).replace(".",",");
+            }
+
+            calculatePriceButton.disabled = false;
+            spinnerElement.classList.toggle('d-none');
+            statusText.innerHTML = originalStatusText;
+    
+            result.innerHTML = `Il prezzo finale è di € ${ticketPrice}`;
+        }, 2000);
     }
-
-    result.innerHTML = `Il prezzo finale è di € ${ticketPrice}`;
 });
