0d3941ced07428fd36d1daeffcb1162629c1ed84
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2024-12-18 18:49:17 +0100

    feat: main features implemented

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
 let kmToCommute = document.querySelector(".kmToCommute");
-let age = document.querySelector(".age")
+let age = document.querySelector(".age");
 const calculatePriceButton = document.querySelector(".calculatePriceButton");
-let finalPrice = document.querySelector(".result");
+let ticketPrice = document.querySelector(".result");
 
-let minorAgeDiscout = 0;
-let elderAgeDiscount = 0;
-let ticketPrice = 0;
+let underAgeDiscout = 0;
+let over65Discount = 0;
 let result = 0;
 const pricePerKm = 0.21;
 
 calculatePriceButton.addEventListener('click', function(event){
     event.preventDefault();
 
+    kmToCommute = parseFloat(kmToCommute.value);
+    console.log(kmToCommute);
+
     if(isNaN(kmToCommute)){
         console.log("valore non valido");
-        alert("Inserisci un numero per favore");
+        alert("Inserisci un numero per favore"); // TODO design this element
     } else {
-        kmToCommute = Number(kmToCommute);
-        console.log("Km da percorrere: ",kmToCommute);
+        // console.log("Km da percorrere: ",kmToCommute);
         ticketPrice = pricePerKm * kmToCommute;
-        console.log("Prezzo del biglietto: ",ticketPrice);
+        // console.log("Prezzo del biglietto: ",ticketPrice);
+    
 
-        
+        // price applied for underage and elderly people
+        if(age.value === "underAge") {
+            underAgeDiscout = ticketPrice - ((ticketPrice * 20) / 100);
+            ticketPrice = underAgeDiscout.toFixed(2).replace(".",",");
+            console.log((`Il prezzo finale è di € ${ticketPrice}`));
+        } else if (age.value === "over65"){
+            over65Discount = ticketPrice - ((ticketPrice * 40) / 100);
+            ticketPrice = over65Discount.toFixed(2).replace(".",",");
+            console.log((`Il prezzo finale è di € ${ticketPrice}`));
+        } else {
+            ticketPrice = ticketPrice.toFixed(2).replace(".",",");
+            console.log((`Il prezzo finale è di € ${ticketPrice}`));
+        }
     }
-
 });
