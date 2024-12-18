a9def0e08c4c1e808fa457b32231c9ac032dee36
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2024-12-18 15:57:01 +0100

    feat: initial setup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let kmToCommute = document.querySelector("kmToCommute");
+let age = document.querySelector("age")
+
+let minorAgeDiscout = 0;
+let elderAgeDiscount = 0;
+let ticketPrice = 0;
+let finalPrice = 0;
+const pricePerKm = 0.21;
+
